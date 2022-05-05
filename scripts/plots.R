56efa8d35b02c009fb98ac1fe453540c73751c90
Gregory Milne <gmilne@rvc.ac.uk>
2022-05-05 10:54:47 +0100

    add basic summary stats calculations & improve graph appearance

--- scripts/plots.R
+++ scripts/plots.R
@@ -11,49 +11,59 @@ library(scales)  #to show colour palette
 ## Read in cleaned data
 dat <- readRDS("data/cleandat.RDS")
 
-## Check World Bank income status (as of 2021) of countries in dataset
+## Basic summary stats:
+## (1) How many people in the dataset?
+sum(dat$n[which(!is.na(dat$n))]) + 2209  #2209 is n for QC, Canada (n not available separately for each timepoint)
+
+## (2) Years over which the data were collected?
+print(paste(min(dat$sampleyr1), sort(dat$sampleyr2)[length(sort(dat$sampleyr2))], sep="-"))
+
+## (3) World Bank income statuses of countries in the dataset (as of 2021)?  
 wb <- read.csv("data/WB21-data.csv", fileEncoding="UTF-8-BOM")
 
-# Make country names comparable between datasets
+# make country names comparable between datasets
 sorted_countries           <- as.character(sort(unique(dat$country)))
-sorted_countries[c(3, 12)] <- "United Kingdom"
-sorted_countries[13]       <- "Korea, Rep."
-sorted_countries[16]       <- "United States"
+sorted_countries[c(which(sorted_countries == "England"), 
+                   which(sorted_countries == "Scotland"))] <- "United Kingdom"
+sorted_countries[which(sorted_countries == "South Korea")] <- "Korea, Rep."
+sorted_countries[which(sorted_countries == "USA")]         <- "United States"
 
-# Find income status for each country
+# find income status for each country
 incomes             <- vector("character", length(sorted_countries))
 
 for (i in 1:length(sorted_countries)) {
   incomes[i] <- wb$Income.group [ which(wb$Country == sorted_countries[i])]
 }
 
-# How many countries in each income bracket?
+# how many countries in each income bracket?
 length(incomes [which(incomes == "High income")])          #how many high income countries?
 length(incomes [which(incomes == "Upper middle income")])  #how many upper-middle income countries?
 
 ## Create colour scale for plots
 dat$country <- as.factor(dat$country)
-nlevels(dat$country)  #how many countries?
+nlevels(dat$country)   #how many countries in the dataset?
 
 # https://medialab.github.io/iwanthue/
-palette <- c("#bf6376",
-             "#d54146",
-             "#c56659",
-             "#d86222",
-             "#bf773b",
-             "#a78858",
-             "#c5ad2e",
-             "#92903c",
-             "#62b242",
-             "#5a9f5d",
-             "#45af9c",
-             "#7689c8",
-             "#7069d5",
-             "#ba5cc0",
-             "#bf6ca4",
-             "#d04080")
-
-show_col(palette)
+palette <- c("#bf4a5c",
+             "#cc5339",
+             "#a04730",
+             "#c28140",
+             "#d1972c",
+             "#9f9743",
+             "#99b43f",
+             "#54903d",
+             "#59bf7a",
+             "#43c8ac",
+             "#3ba7e5",
+             "#5e7ecb",
+             "#6f71d9",
+             "#503183",
+             "#9d6dbf",
+             "#d88ad2",
+             "#bf55b2",
+             "#892961",
+             "#d76092")
+# show_col(palette)  #visualise palette
 
 names(palette) <- levels(dat$country)  #create fixed factor colours
 my_scale <- scale_colour_manual(name = "Country", values = palette, limits=force)  #specify scale_fill_manual
@@ -69,11 +79,11 @@ point_size <- 1.2
 ## Plots for pregnant women
 p[[1]] <- dat %>%
   filter(population == "Pregnant women") %>%
-  ggplot(aes(x=medyr, y=prev, col=country)) + 
+  ggplot(aes(x=medyr, y=prev, col=country, line=region, shape=national)) + 
   geom_point(size=point_size) +
   geom_errorbar(aes(ymin=low_ci, ymax=up_ci), width=0.3) + 
   geom_smooth(method="lm", se=F, size=line_size) +
-  labs(title="Pregnant women", x="Year", y="Seroprevalence (%)") + 
+  labs(title="Pregnant women", x="Year", y="Seroprevalence (%)", shape="Region") + 
   scale_x_continuous(limits=c(1960, 2020), breaks=seq(1960, 2020, 20), expand = c(0,0)) + 
   scale_y_continuous(limits=c(0, 75), breaks=seq(0,75,25), expand = c(0,0)) + 
   theme_light() +
@@ -82,11 +92,11 @@ p[[1]] <- dat %>%
 ## Plots for childbearing-age women
 p[[2]] <- dat %>%
   filter(population == "Childbearing-age women") %>%
-  ggplot(aes(x=medyr, y=prev, col=country)) + 
+  ggplot(aes(x=medyr, y=prev, col=country,  shape=national)) + 
   geom_point(size=point_size) +
   geom_errorbar(aes(ymin=low_ci, ymax=up_ci), width=0.3) + 
   geom_smooth(method="lm", se=F, size=line_size) +
-  labs(title="Childbearing-age women", x="Year", y="Seroprevalence (%)") + 
+  labs(title="Childbearing-age women", x="Year", y="Seroprevalence (%)", shape="Region") + 
   scale_x_continuous(limits=c(1960, 2020), breaks=seq(1960, 2020, 20), expand = c(0,0)) + 
   scale_y_continuous(limits=c(0, 75), breaks=seq(0,75,25), expand = c(0,0)) + 
   theme_light() +
@@ -96,25 +106,25 @@ p[[2]] <- dat %>%
 ## Plots for general population
 p[[3]] <- dat %>%
   filter(population == "General population") %>%
-  ggplot(aes(x=medyr, y=prev, col=country, shape=national)) + 
+  ggplot(aes(x=medyr, y=prev, col=country, line=region, shape=national)) + 
   geom_point(size=point_size) + 
   geom_errorbar(aes(ymin=low_ci, ymax=up_ci), width=0.3) + 
-  geom_smooth(method="lm", se=F, size=line_size) +
+  geom_smooth(method="lm", se=F, size=line_size, formula = y~x) +
   labs(title="General population", x="Year", y="Seroprevalence (%)", shape="Region") + 
   scale_x_continuous(limits=c(1960, 2020), breaks=seq(1960, 2020, 20), expand = c(0,0)) + 
   scale_y_continuous(limits=c(0, 75), breaks=seq(0,75,25), expand = c(0,0)) + 
   theme_light() + 
   my_scale
   
-
+  
 ## Plots for blood donors
 p[[4]] <- dat %>%
   filter(population == "Blood donors") %>%
-  ggplot(aes(x=medyr, y=prev, col=country)) + 
+  ggplot(aes(x=medyr, y=prev, col=country, line=region, shape=national)) + 
   geom_point(size=point_size) + 
   geom_errorbar(aes(ymin=low_ci, ymax=up_ci), width=0.3) + 
   geom_smooth(method="lm", se=F, size=line_size) +
-  labs(title="Blood donors", x="Year", y="Seroprevalence (%)") + 
+  labs(title="Blood donors", x="Year", y="Seroprevalence (%)", shape="Region") + 
   scale_x_continuous(limits=c(1960, 2020), breaks=seq(1960, 2020, 20), expand = c(0,0)) + 
   scale_y_continuous(limits=c(0, 75), breaks=seq(0,75,25), expand = c(0,0)) + 
   theme_light() + 
@@ -149,8 +159,8 @@ p[[6]] <- dat %>%
 
 # population multipanel plot
 wrap_plots(p, ncol=2) &  
-  plot_annotation(tag_levels = list(c('(a)', '(b)', '(c)', '(d)', '(e)', '(f)')))
-  
+  plot_annotation(tag_levels = list(c('(a)', '(b)', '(c)', '(d)', '(e)', '(f)'))) & 
+  theme(legend.spacing.y = unit(0.05, "cm"))  #reduce space between fig legends
 
 ## Save
 # PDF
