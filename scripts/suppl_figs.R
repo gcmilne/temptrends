dcdb46bd80e2faa321d89261c1641c88b9917cdd
Gregory Milne <gmilne@rvc.ac.uk>
2022-09-05 16:13:30 +0100

    adjust age-seroprev graph

--- scripts/suppl_figs.R
+++ scripts/suppl_figs.R
@@ -9,15 +9,12 @@ library("dplyr")
 ## Read in data
 df <- readRDS("data/CTout.RDS")
 
-plot(df[[1]]$age, df[[1]]$prev, 'l', ylim=c(-0.1, 1.1),  xlab="Age (years)", ylab="Seroprevalence")
-for (k in 2:nrow(out)) lines(df[[k]]$age, df[[k]]$prev)
-
 # Plot
 ggplot(bind_rows(df, .id="data_frame"), aes(x=age, y=prev*100, group=data_frame)) +
   geom_line(alpha=0.45) + 
   labs(x="Age (years)", y="Seroprevalence (%)") + 
   scale_x_continuous(limits=c(0, 60), breaks=seq(0, 60, 20), expand = c(0,0)) +
-  scale_y_continuous(expand=c(0,0)) +
+  scale_y_continuous(limits=c(0, 100), breaks=seq(0, 100, 25), expand=c(0,0)) +
   theme_light()
 
 ## Save plot
